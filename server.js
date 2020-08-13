d9ad42e544b4d34bc2868d582c06f53593052447
Jvictor23 <joaovictor00788@hotmail.com>
2020-08-13 20:44:15 -0300

    Adicionando express

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const configs = {
 //             next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
 //     });
 
-app.use(express.static(path.join(__dirname, configs.caminho))); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
+app.use(express.static(path.join(__dirname, 'public'))); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
 
 app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
     const index = path.join(__dirname, configs.caminho, "index.html");
