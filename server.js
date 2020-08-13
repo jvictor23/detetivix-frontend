d6edf81596b96153bbf7c3e1798e3b61836f4a63
Jvictor23 <joaovictor00788@hotmail.com>
2020-08-13 18:52:26 -0300

    Adicionando express

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, regist
 app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
 
 app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
-    res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
+const index = path.join(__dirname, configs.caminho, "index.html"
+console.log(index)    
+// res.sendFile());
 });
 
 app.listen(configs.port, () => {
