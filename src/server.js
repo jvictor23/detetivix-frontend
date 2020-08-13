f40960dc83f0caf114a36f61afc9df7c5b222289
Jvictor23 <joaovictor00788@hotmail.com>
2020-08-13 18:27:39 -0300

    Adicionando express

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 
 const configs = {
-    caminho: "", //Aqui será definido a pasta de saída onde contém o index.html e os outros arquivos.
+    caminho: "build", //Aqui será definido a pasta de saída onde contém o index.html e os outros arquivos.
     forcarHTTPS: false, //Defina para true se desejar que o redirecionamento para HTTPS seja forçado (é necessário certificado SSL ativo)
     port: process.env.PORT || 3000
 }
@@ -19,7 +19,7 @@ if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, regist
 app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
 
 app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
-    res.sendFile(path.join(__dirname, "index.js"));
+    res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
 });
 
 app.listen(configs.port, () => {
