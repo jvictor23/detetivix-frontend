2cd21753df435b36fa85776c504a28996e7ecf79
Jvictor23 <joaovictor00788@hotmail.com>
2020-08-13 20:42:53 -0300

    Adicionando express

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const configs = {
     port: process.env.PORT || 3000
 }
 
-if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
-    app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
-        if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
-            res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
-        else //Se a requisição já é HTTPS
-            next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
-    });
+// if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
+//     app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+//         if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+//             res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
+//         else //Se a requisição já é HTTPS
+//             next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+//     });
 
-app.use(express.static('public')); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
+app.use(express.static(path.join(__dirname, configs.caminho))); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
 
 app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
     const index = path.join(__dirname, configs.caminho, "index.html");
